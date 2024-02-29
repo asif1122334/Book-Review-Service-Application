e317afab36c857315829af007cf9c6da23b32352
asif <asif@gmail.com>
2024-02-29 16:33:12 +0530

    first commit..

---
+++ BookReviewServiceEureka-1/src/main/java/com/mobilefirst/bookreview/BookReviewServiceEureka1Application.java
@@ -0,0 +1,15 @@
+package com.mobilefirst.bookreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BookReviewServiceEureka1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewServiceEureka1Application.class, args);
+	}
+
+}
---
+++ BookReviewServiceEureka-1/src/test/java/com/mobilefirst/bookreview/BookReviewServiceEureka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilefirst.bookreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewServiceEureka1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
